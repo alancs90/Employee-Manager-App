18f6e1a1f4868ab6617e17e791c162c587f6938c
Alan Carrasco <alancs90@gmail.com>
2022-02-24 19:23:55 -0500

    auto-update 2022-02-24-19:23:55

--- employeemanager/src/main/java/com/alan/employeemanager/model/Employee.java
+++ employeemanager/src/main/java/com/alan/employeemanager/model/Employee.java
@@ -85,7 +85,6 @@ public class Employee implements Serializable {
     public void setEmployeeCode(String employeeCode) {
         this.employeeCode = employeeCode;
     }
-    
 
     @Override
     public String toString() {
