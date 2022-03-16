40057a70bab58368cd4452774d01778916ec3793
Alan Carrasco <alancs90@gmail.com>
2022-03-16 00:04:30 -0400

    auto-update 2022-03-16-00:04:30

--- employeemanager/src/test/java/com/alan/employeemanager/EmployeeResourceTest.java
+++ employeemanager/src/test/java/com/alan/employeemanager/EmployeeResourceTest.java
@@ -1,13 +1,39 @@
 package com.alan.employeemanager;
 
+import com.alan.employeemanager.model.Employee;
+import com.alan.employeemanager.service.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeResourceTest {
+
+    @Mock
+    private EmployeeService mockEmployeeService;
+
+    @InjectMocks
+    private EmployeeResource mockEmployeeResource;
 
     @Test
     void getAllEmployees() {
+
+        when(mockEmployeeService.findAllEmployees()).thenReturn(new ArrayList<Employee>());
+        ResponseEntity<List<Employee>> actualResponse = mockEmployeeResource.getAllEmployees();
+        ResponseEntity<List<Employee>> expectedResponse = new ResponseEntity<>(new ArrayList<Employee>(), HttpStatus.OK);
+
+        assertEquals(expectedResponse, actualResponse);
     }
 
     @Test
